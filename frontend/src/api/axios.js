e6d3868fb91c7de84d3a700bfc8016f914b79c5c
Rahuls2642 <135409027+Rahuls2642@users.noreply.github.com>
2025-11-05 17:45:11 +0530

    Change API base URL to production endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,6 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:"https://movie-cine-backend-1.onrender.com/api/",
-  baseURL: "http://localhost:3000/api",
   withCredentials: false,
 });
 
