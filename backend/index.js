299113692023e7f8e561f1e9c7bd1fc653319459
Rahuls2642 <135409027+Rahuls2642@users.noreply.github.com>
2025-11-05 17:38:14 +0530

    Update CORS configuration for production

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:5173' ,
-  origin:'http://localhost:5174'
+  origin:'https://movie-cine-frontend-jp1t.onrender.com'
 }))
 
 app.get('/', (req, res) => {
